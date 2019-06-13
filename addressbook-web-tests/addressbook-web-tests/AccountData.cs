51fdf413447aa0c232074984b4ed0dd8601c3aa8
Дмитирий Бессонов <dimbizon@gmail.com>
2019-06-13 21:47:31 +0300

    Задание №3: Создать вспомогательные методы и классы

---
+++ addressbook-web-tests/addressbook-web-tests/AccountData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class AccountData
+    {
+        private string username;
+        private string password;
+        
+        public AccountData(string username, string password) // конструктор
+        {
+            this.username = username; // присваиваем значение, которое передано как параметр
+            this.password = password;
+        }
+        public string Username // свойство
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password // свойство
+        {
+            get // возвращает
+            {
+                return password;
+            }
+            set // присваивает
+            {
+                password = value;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class GroupData
+    {
+        private string name;
+        private string header = "";
+        private string footer = "";
+        public GroupData(string name) // конструктор
+        {
+            this.name = name;
+        }
+
+        public string Name // св-во
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Header // св-во
+        {
+            get
+            {
+                return header;
+            }
+            set
+            {
+                header = value;
+            }
+        }
+
+        public string Footer // св-во
+        {
+            get
+            {
+                return footer;
+            }
+            set
+            {
+                footer = value;
+            }
+        }
+    }
+}
