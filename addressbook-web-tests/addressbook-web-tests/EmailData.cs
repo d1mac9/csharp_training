c663e48f5acdc99822f3908597385827f232623d
Дмитирий Бессонов <dimbizon@gmail.com>
2019-06-16 19:32:24 +0300

    Задание №4: Сделать всё то же самое, но для контактов

---
+++ addressbook-web-tests/addressbook-web-tests/EmailData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+
+{
+    class EmailData
+    {
+        private string email;
+        private string email2 = "";
+        private string email3 = "";
+
+        public EmailData(string email) // конструктор
+        {
+            this.email = email;
+        }
+        public string Email // св-во
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string Email2 // св-во
+        {
+            get
+            {
+                return email2;
+            }
+            set
+            {
+                email2 = value;
+            }
+        }
+
+        public string Email3 // св-во
+        {
+            get
+            {
+                return email3;
+            }
+            set
+            {
+                email3 = value;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/FullName.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class FullName
+    {
+        private string firstname;
+        private string middlename = "";
+        private string lastname;
+        public FullName(string firstname, string lastname) // конструктор
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        public string Firstname // св-во
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Middlename // св-во
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+
+        public string Lastname // св-во
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
