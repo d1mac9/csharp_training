36b46db2a6a9c6c0e51cb702197f80c0bc1ce73a
Дмитирий Бессонов <dimbizon@gmail.com>
2019-07-04 22:55:59 +0300

    Лекция 4.1. Коллекции (списки)

---
+++ addressbook-web-tests/addressbook-web-tests/Tests/UnitTest2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook_web_tests.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = null;
+            int attempt = 0;
+
+            do
+            {
+                System.Threading.Thread.Sleep(1000);
+                attempt++;
+            }
+            while (driver.FindElements(By.Id("Test")).Count == 0 && attempt < 60);
+        }
+    }
+}
